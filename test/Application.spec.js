d4f12dabf1930ae8793e03d97cc6f292060ae1be
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-09-22 09:01:39 -0600

    fixing mount test

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -4,40 +4,40 @@ import { assert, expect } from 'chai';
 
 import Application from '../lib/components/Application';
 
-describe('Application', () => {
+describe('Application', function() {
 
-  it('renders as a <div>', () => {
-    const wrapper = shallow(<Application />)
+  it('renders as a <div>', function() {
+    const wrapper = shallow(<Application />);
     assert.equal(wrapper.type(), 'div');
   });
 
-  it('should allow us to set messages array as state', () => {
+  it('should allow us to set messages array as state', function() {
     const wrapper = mount(<Application />);
     wrapper.state().messages = ['displayName', 'email', 'uid'];
     expect(wrapper.state().messages.length).to.equal(3);
   });
 
-  it('should allow us to set a draftMessages array as state', () => {
+  it('should allow us to set a draftMessages array as state', function() {
     const wrapper = mount(<Application />);
 
     wrapper.setState({ draftMessage: "I'm into it."});
     assert.equal(wrapper.state('draftMessage'), "I'm into it.");
   });
 
-  it('should allow us to set a user state ', () => {
+  it('should allow us to set a user state ', function() {
     const wrapper = mount(<Application />);
     expect(wrapper.state.user).to.equal(undefined);
     wrapper.state().user = 'Paul';
     expect(wrapper.state().user).to.equal('Paul');
   });
 
-  it('should allow us to set a the sort state', () => {
+  it('should allow us to set a the sort state', function() {
     const wrapper = mount(<Application />);
     wrapper.state().userIsFiltered = false;
     expect(wrapper.state().userIsFiltered).to.equal(false);
   });
 
-  it('should allow us to set filteredMessages array as state', () => {
+  it('should allow us to set filteredMessages array as state', function() {
     const wrapper = mount(<Application />);
     wrapper.state().filteredMessages = ['displayName', 'email', 'uid'];
     expect(wrapper.state().filteredMessages.length).to.equal(3);
--- test/Sort-spec.js
+++ test/Sort-spec.js
@@ -8,17 +8,46 @@ import Header from '../lib/components/Header';
 import Button from '../lib/components/Button';
 
 describe('Sort Messages', function() {
-  it('should renders as a button', () => {
+  let message1 = {
+    key: "-KS3GtKnWB_XZKjgb0iJ",
+    content: "Scrbabble",
+    createdAt: 1474319324550,
+    user: {
+        displayName: "Paul Nguyen",
+        email: "helloitsme@gmail.com",
+        uid: "oSMIlUTN5OZOVY9pYfEi1PXCv9q1"
+    }
+  };
+
+  let message2 = {
+    key: "-KS3ONDVgCurMmG_dP4-",
+    content: "testing out a new message",
+    createdAt: 1474321285995,
+    user: {
+        displayName: "MaryJane Valade",
+        email: "helloitsyou@gmail.com",
+        uid: "8opUzbSXdxOJcKgukMF4oIMPTu12"
+    }
+  };
+
+  it('should renders as a button', function() {
   const wrapper = shallow(<Button /> );
   assert.equal(wrapper.type(), 'button');
   });
 
-  it('should render text inside of the button', () => {
+  it('should render text inside of the button', function(){
     const wrapper = shallow(<Button text='Sort'/> );
     expect(wrapper.contains('Sort')).to.equal(true);
   });
 
-  
+  it('should render the messages in reverse chronological order', function() {
+    const wrapper = mount(<Application />);
+    wrapper.state().messages = [message1, message2];
+    // wrapper.state().user = message.user.displayName
+
+    let button = wrapper.find('.button-sort').simulate('click')
+    expect(wrapper.state().messages).to.deep.equal([message2, message1]);
+  });
 
 
 });
